538b9c9a340d8e2a24d775f2bc68c74ad21dc7b1
L G JAYAVEL <147837290+Jayavel2005@users.noreply.github.com>
2026-09-22 11:08:16 +0530

    Time: 0 ms (100%), Space: 43.4 MB (61.07%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int k = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                swap(i, k++, nums);
+            }
+        }
+        return k;
+    }
+    public void swap(int k, int i, int[] nums) {
+        int temp = nums[i];
+        nums[i] = nums[k];
+        nums[k] = temp;
+    }
+}
